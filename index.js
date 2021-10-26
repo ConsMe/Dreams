79870cab7849d833f43069989824c31ddeee79ca
Consme <odn29@yandex.ru>
2021-10-26 23:34:28 +0300

    next commit

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const {spawn} = require('child_process');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
+app.use(express.static('public'))
+
+app.get('/api', (req, res) => {
  
  var dataToSend;
  // spawn new child process to call the python script
 //  const python = spawn('python3', ['record.py']);
- const python = spawn('lsusb');
+//  const python = spawn('lsusb');
+ let python;
+ const comm = req.body.split(' ');
+ if (comm.length === 1) {
+   python = spawn(comm[0]);
+ } else {
+   python = spawn(comm[0], comm.slice(1));
+ }
  // collect data from script
  python.stdout.on('data', function (data) {
   console.log('Pipe data from python script ...');
